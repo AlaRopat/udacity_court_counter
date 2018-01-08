ce32fced60e6801c6fb5f0447f92c2ff62d8a173
ala_ropat <ala_ropat@epam.com>
2018-01-08 15:05:52 +0300

    add get result button

--- app/src/main/java/com/example/admin/secondpracticeset/MainActivity.java
+++ app/src/main/java/com/example/admin/secondpracticeset/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.admin.secondpracticeset;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -11,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     private static final int FREE_THROW = 1;
     private static int scoreTeamA = 0;
     private static int scoreTeamB = 0;
+    private static final String RESULT_MESSAGE = "The %s won !!!";
+    private static final String GAME_NOT_STARTED = "The Game didn't start";
+    private static final String TEAM_A = "Team A";
+    private static final String TEAM_B = "Team B";
+    private static final String DEAD_HEAT = "Dead heat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -108,5 +113,30 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamB = 0;
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method displays a game result
+     *
+     * @param resultMessage
+     */
+    private void displayWinMessage(String resultMessage) {
+        TextView resultTextView = findViewById(R.id.result_text_view);
+        resultTextView.setText(resultMessage);
+    }
+
+    /**
+     * This method calculate a game result and reset the score text views
+     *
+     * @param v
+     */
+    public void getResult(View v) {
+        if (scoreTeamB == 0 && scoreTeamA == 0) {
+            displayWinMessage(GAME_NOT_STARTED);
+            return;
+        }
+        String resultMessage = (scoreTeamA > scoreTeamB) ? String.format(RESULT_MESSAGE, TEAM_A) : (scoreTeamB == scoreTeamA) ? DEAD_HEAT : String.format(RESULT_MESSAGE, TEAM_B);
+        displayWinMessage(resultMessage);
+        resetScores(v);
     }
 }
